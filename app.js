0d86431f3938ac00babee51557f274c04a4c7788
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-06-30 17:19:18 +0700

    decrypt

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  input = document.getElementById("plaintext"),
-  result = document.getElementById("result_cipher");
+  plaintext = document.getElementById("plaintext"),
+  ciphertext = document.getElementById("ciphertext");
 
 function caesarCipher(message, shift) {
   var output = "";
@@ -26,22 +26,22 @@ function caesarCipher(message, shift) {
 }
 
 function RailFenceEncrypt(pesan, kunci) {
-  plaintext = pesan.toLowerCase().replace(/[^a-z]/g, "");
+  plain = pesan.toLowerCase().replace(/[^a-z]/g, "");
   var key = parseInt(kunci);
-  ciphertext = "";
+  cipher = "";
   for (line = 0; line < key - 1; line++) {
     skip = 2 * (key - line - 1);
     j = 0;
-    for (i = line; i < plaintext.length; ) {
-      ciphertext += plaintext.charAt(i);
+    for (i = line; i < plain.length; ) {
+      cipher += plain.charAt(i);
       if (line == 0 || j % 2 == 0) i += skip;
       else i += 2 * (key - 1) - skip;
       j++;
     }
   }
-  for (i = line; i < plaintext.length; i += 2 * (key - 1))
-    ciphertext += plaintext.charAt(i);
-  return ciphertext;
+  for (i = line; i < plain.length; i += 2 * (key - 1))
+    cipher += plain.charAt(i);
+  return cipher;
 }
 
 var Vigenere = (function () {
@@ -93,12 +93,69 @@ function updateValue(e) {
   shift = document.getElementById("key_caesarcipher").value;
   kunci_railfence = document.getElementById("key_railfence").value;
   kunci_vigenere = document.getElementById("key_vigenere").value;
-  result.value = Vigenere.encrypt(
+  ciphertext.value = Vigenere.encrypt(
     RailFenceEncrypt(
       caesarCipher(e.target.value, parseInt(shift)),
-      kunci_railfence
+      parseInt(kunci_railfence)
     ),
     kunci_vigenere,
     true
-    );
+  );
+}
+
+function RailFenceDecrypt(pesan, kunci) {
+  cipher = pesan.toLowerCase().replace(/[^a-z]/g, "");
+  var key = parseInt(kunci);
+  pt = new Array(cipher.length);
+  k = 0;
+  for (line = 0; line < key - 1; line++) {
+    skip = 2 * (key - line - 1);
+    j = 0;
+    for (i = line; i < cipher.length; ) {
+      pt[i] = cipher.charAt(k++);
+      if (line == 0 || j % 2 == 0) i += skip;
+      else i += 2 * (key - 1) - skip;
+      j++;
+    }
+  }
+  for (i = line; i < cipher.length; i += 2 * (key - 1))
+    pt[i] = cipher.charAt(k++);
+  return pt.join("");
+}
+
+function caesarCipherDecrypt(message, shift) {
+  var output = "";
+
+  message = message.toUpperCase();
+
+  while (shift > 0) {
+    shift -= 26;
+  }
+
+  for (var i in message) {
+    var c = message[i],
+      index = alpha.indexOf(c);
+
+    if (index >= 0) {
+      var shiftLetter = alpha[(index + shift) % 26];
+      output += shiftLetter;
+    } else {
+      output += c;
+    }
+  }
+  return output;
+}
+
+ciphertext.addEventListener("input", update);
+function update(e) {
+  shift = document.getElementById("key_caesarcipher").value;
+  kunci_railfence = document.getElementById("key_railfence").value;
+  kunci_vigenere = document.getElementById("key_vigenere").value;
+  plaintext.value = caesarCipherDecrypt(
+    RailFenceDecrypt(
+      Vigenere.decrypt(e.target.value, kunci_vigenere, true),
+      parseInt(kunci_railfence)
+    ),
+    parseInt(shift)
+  );
 }
